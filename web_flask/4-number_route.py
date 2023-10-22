abdee942b9c2c56cbc8bae772d32d3fa97eb6c09
billwk254 <billkihungu51@gmail.com>
2023-10-22 19:55:01 +0300

    Flask application with several routes defined

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application.
+
+- Listens on 0.0.0.0, port 5000.
+- Defines five routes:
+    1. '/': Displays "Hello HBNB!"
+    2. '/hbnb': Displays "HBNB"
+    3. '/c/<text>': Displays "C " followed by the value of the text variable, replacing underscores with spaces.
+    4. '/python/(<text>)': Displays "Python " followed by the value of the text variable, or "Python is cool" if no text is provided.
+    5. '/number/<n>': Displays "n is a number" only if n is an integer.
+
+Usage:
+1. Run this script to start the web application.
+2. Access the routes with a web browser or using curl.
+
+"""
+
+from flask import Flask
+from urllib.parse import unquote
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays "Hello HBNB!" when accessing the root route.
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays "HBNB" when accessing the '/hbnb' route.
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """
+    Displays "C " followed by the value of the text variable, replacing underscores with spaces.
+
+    :param text: The text to be displayed.
+    """
+    text = unquote(text).replace('_', ' ')
+    return f"C {text}"
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text):
+    """
+    Displays "Python " followed by the value of the text variable, or "Python is cool" if no text is provided.
+
+    :param text: The text to be displayed.
+    """
+    text = unquote(text).replace('_', ' ')
+    return f"Python {text}"
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """
+    Displays "n is a number" when accessing the '/number/<n>' route, but only if n is an integer.
+
+    :param n: The number to be displayed.
+    """
+    return f"{n} is a number"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
